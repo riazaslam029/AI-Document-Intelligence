bf15408238707839e8ce345231b058a650e90547
Riaz Aslam <riazaslam029@gmail.com>
2026-07-03 22:44:40 +0500

    Fix Streamlit Cloud OCR dependencies

--- utils/ocr.py
+++ utils/ocr.py
@@ -1,13 +1,14 @@
-import easyocr
 
-reader = easyocr.Reader(["en"])
+
+
 
 
 def extract_image_text(image_path):
     """
     Extract text from an image using EasyOCR.
     """
-
+    import easyocr
+    reader = easyocr.Reader(["en"])
     results = reader.readtext(image_path)
 
     text = "\n".join([item[1] for item in results])
